3a65bca66e3a418e5eb9c782942e25063b166016
Luis Miranda Calderón <luismirandac92@gmail.com>
2025-06-29 19:53:10 -0600

    mm

--- src/ui/ContenidoCambiante.jsx
+++ src/ui/ContenidoCambiante.jsx
@@ -4,29 +4,30 @@ import Box from "@mui/material/Box";
 export const ContenidoCambiante = ({titulo}) => {
   return (
     <>
-       <Box
-            sx={{
-                position: 'fixed',
-                top: 0,
-                right: { xs:0,sm:'6vw'},
-                marginTop:9,
-                width: { xs: '80px', sm: '120px' }, // ancho responsive
-                height: { xs: '80px', sm: '120px' }, // alto responsive
-                backgroundColor: 'black',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                color: 'white',
-                borderTopLeftRadius: '12px',
-                zIndex: 1000,
-                fontWeight:"bold",
-                fontSize: { xs: '0.5rem', sm: '0.9rem' },
-                textAlign: 'center',
-                padding:1
-            }}
-        >
-            Recorrido dentro de nuestro bosque cambiante. {titulo}
-        </Box>
+        <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        right: { xs: 0, sm: '6vw' },
+        marginTop: 9,
+        backgroundColor: 'black',
+        display: 'flex',
+        flexDirection: 'column', // para apilar el texto principal y secundario
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        borderTopLeftRadius: '12px',
+        zIndex: 1000,
+        fontWeight: 'bold',
+        fontSize: { xs: '0.6rem', sm: '0.9rem' },
+        textAlign: 'center',
+        padding: 1,
+        maxWidth: '10vw',
+      }}
+    >
+      <span>Recorrido dentro de nuestro bosque cambiante. {titulo}</span>
+      <span style={{ fontSize: '0.7rem', fontWeight: 400, marginTop: 4 }}>Día Superquantium</span>
+    </Box>
     </>
   )
 }
